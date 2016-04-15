aa9da64c17e267eaf12b6e6fa7c3ae23f204cb5f
azemoh <joshazemoh@gmail.com>
2016-04-15 02:06:31 +0100

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,13 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
 
+  def new
+  end
+
+  def create
+    @post = Post.create(title: params[:title], description: params[:description])
+    redirect_to posts_path
+  end
 end
