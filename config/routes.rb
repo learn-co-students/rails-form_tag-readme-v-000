085a9d9353f8481c60c8e8ad9f53f579eeea71cd
Motti Mordechay Gottlieb <mottigottlieb@gmail.com>
2016-07-26 03:37:24 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create]
 end
+
+# get "/posts"
+#   erb :"views/posts/index.html.erb"
+# end
+
+#
+# Prefix Verb URI Pattern          Controller#Action
+#   posts GET  /posts(.:format)     posts#index
+# new_post GET  /posts/new(.:format) posts#new
