190a836a8306dbcd5fb7093d2fbc2fe10e110078
Anna Lansfjord <lansfjord@gmail.com>
2017-02-16 21:07:55 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
-  
+
+def new
+end
+
+def create
+  @post = params
+  redirect_to new_post_path(post: @post)
+end
+
 end
