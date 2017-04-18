4869f55340918a3a412be5d928dfce290c319f4a
Daniel Lempesis <danlrcn@hotmail.com>
2017-04-18 15:31:26 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create] #==> index_post_path, new_post_path ( GET /posts/index; GET /posts/new; POST /posts/new )
 end
