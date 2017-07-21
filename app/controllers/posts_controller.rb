04c1e7867f2708c3a306e207c360c78f2a30627b
github username <github email address>
2017-07-20 23:13:11 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,21 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    if Post.create(post_params)
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+   def post_params
+    params.require(:post).permit(:title, :description)
+   end
   
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 describe 'new post' do
+  it 'ensures forms route works with /new action' do
+    visit new_post_path
+    expect(page.status_code).to eq(200)
+  end
 
+  it 'displays a new post form that redirects to an updated index page including new post' do
+    visit new_post_path
+    fill_in 'post_title', with: "My post title"
+    fill_in 'post_description', with: "My post description"
+    click_on 'Submit Post'
+    expect(page.current_path).to eq(posts_path)
+    expect(page).to have_content('My post title')
+    expect(page).to have_content('My post description')
+  end
 end
+
