253410663536bea46a41fd5bf69caba3e2d2b2bb
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-31 12:54:39 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,18 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
-  
+
+  def new
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    # Now this is where, instead of saying redirect '/posts/show' we can 
+    # use our posts_path, which maps this back to the posts#index 
+    redirect_to posts_path
+  end 
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create]
+# Adding :new causes an 'The action 'new' could not be found' error
+# Go to PostsController and add 'new' action (or method)  
 end
