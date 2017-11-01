4d8db63eefad6fa1632b86cc1501394003dfd154
github username <github email address>
2017-11-01 17:07:49 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,26 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def new # implicitly renders the views/posts/new.html.erb view file, which has the same name as this #new action
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
 end
+
+# In the #create controller action (method) above, call AR's #create method on the Post class
+# to instantiate and save to DB a new post instance with its title and description attribute values set via mass assignment
+# and = to whatever the user entered into the title and description form fields,
+# values which are stored in the params hash upon the form's submission
+# the <input> for the form's title field has a name property value = "post[title]", which is a key in the params hash
+# Therefore, whatever the user entered into the title field of the submitted form = params[:post][:title],
+# which becomes the title attribute value of the post instance we're creating
+# the <textarea> for the form's description field has a name property value = "post[description]", which is a key in the params hash
+# Therefore, whatever the user entered into the description field of the submitted form = params[:post][:description],
+# which becomes the description attribute value of the post instance we're creating
+# Finally, we redirect to the index page that displays all posts.
+# The #posts_path route helper method returns "/posts", which is the URL for the link to the index page
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create]
 end
+  # above line of code is the same as saying:
+  # get '/posts', to: 'posts#index'
+  # route is GET request to localhost:3000/posts to display the index of all posts
+  # route is mapped to #index controller action (method) in the PostsController class
+  # By default, the name of the route is posts (since this is the URL path),
+  # so our route helper method is #posts_path, which returns the URL string href attribute value
+  # of the link to the posts index page
+  # line 2 is also the same as saying:
+  # get '/posts/new', to: 'posts#new'
+  # route is GET request to localhost:3000/posts/new, where form is presented to create a new post
+  # route is mapped to #new controller action (method) in PostsController class
+  # By standard RESTful conventions (as seen in Ruby on Rails guides),
+  # the route helper method #new_post_path returns the string URL href attribute value "/posts/new"
+  # of the link to the page where user sees form to create a new post
+  # line 2 is also the same as saying:
+  # post '/posts', to: 'posts#create'
+  # route is POST request to index page '/posts'
+  # route receives the POST request that is sent by the form to create a new post upon its submission
+  # i.e. route receives the data submitted in the form to create a new post
+  # route is mapped to the #create controller action (method) defined in PostsController class
