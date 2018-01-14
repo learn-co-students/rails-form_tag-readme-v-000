8b38784fea39603e7bc72977501401afd0b7c2b9
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-01-14 09:51:27 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :create]
+  get '/new', to: 'posts#new', as: 'new_post'
+  root to: 'posts#index'
 end
