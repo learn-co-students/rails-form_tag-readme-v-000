c7cdff14252746ed32db80a64a2ae6c75059d1cb
Terry-Thompson <terry.lyn.thompson@gmail.com>
2018-07-15 06:27:21 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
+require 'pry'
 class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
   
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
+  def new
+
+  end
+    
 end
