b43eb65a3c30871174090c94ed02486eaff493b6
Barbara Washington <barbara.m.washington@gmail.com>
2018-08-07 20:01:38 +0000

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,11 +7,11 @@ describe 'new post' do
   end
 
   it 'renders HTML in the /new template' do
-      visit new_post_path
-      expect(page).to have_content('Post Form')
-    end
+    visit new_post_path
+    expect(page).to have_content('Post Form')
+  end
 
-    it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
+  it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
     visit new_post_path
     fill_in 'post_title', with: 'My post title'
     fill_in 'post_description', with: 'My post description'
@@ -22,5 +22,4 @@ describe 'new post' do
     expect(page).to have_content('My post title')
     expect(page).to have_content('My post description')
   end
-
 end
