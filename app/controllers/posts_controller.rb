adf2a035d8b2887ab24bed0a064fb4b471869a9a
Christine Cua <cloudycua@gmail.com>
2018-08-21 01:26:59 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,10 @@ class PostsController < ApplicationController
 
   def new
   end
+
+  def create
+    # could also write Post.create(params[:post])
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -18,6 +18,8 @@ describe 'new post' do
     fill_in 'post_title', with: 'My post title'
     fill_in 'post_description', with: 'My post description'
 
+    click_on 'Submit Post'
+
     expect(page.current_path).to eq(posts_path)
     expect(page).to have_content('My post title')
     expect(page).to have_content('My post description')
