577a4aa2d2e095bb8234098d82985a4bc8c7d994
veloblank <veloblank@gmail.com>
2019-03-04 17:42:48 -0500

    More practice. Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,23 +4,13 @@ class PostsController < ApplicationController
   end
 
   def new
-<<<<<<< HEAD
 
-  end
-
-  def create
-    post = Post.create(title: params[:post][:title], description: params[:post][:description])
-    redirect_to posts_path
-  end
-
-
-
-=======
   end
 
   def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
     redirect_to posts_path
   end
->>>>>>> b3077bed0d50c25829a0fa6f95774eff05ebffff
+
+
 end
