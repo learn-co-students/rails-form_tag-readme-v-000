a98c284ad96a0eeea75787bedcaa7226fa8ae49d
esugano <esugano.git@gmail.com>
2019-05-24 14:01:01 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new]
 end
